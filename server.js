456a9dba47526e73f6def713b6de8957c308bdf1
diwanumang <diwan.umang@gmail.com>
2024-04-20 22:03:48 -0400

    Completed lab

--- server.js
+++ server.js
@@ -36,6 +36,32 @@ app.get("/collectibles/:index", (req, res) => {
   if (req.params.index > collectibles.length - 1) {
     res.send('<p>This item is not yet in stock.</p>');
   } else {
-        res.send(`<p>So you want the ${collectibles[req.params.index].name}?</p>`);
+        res.send(`<p>So you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!</p>`);
   }
+});
+
+// 4. Filter Shoes by Query Parameters
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes', (req, res) => {
+    const minPrice = parseInt(req.query['min-price']);
+    const maxPrice = parseInt(req.query['max-price']);
+    const type = req.query.type;
+    const resultShoes = [];
+
+    for (let i = 0; i < shoes.length; i++) {
+        if ((Number.isNaN(minPrice) || minPrice > shoes[i].price) && (Number.isNaN(maxPrice) || maxPrice < shoes[i].price) && (!type || type === shoes[i].type)) {
+            resultShoes.push(shoes[i]);
+        }
+    }
+    res.send(resultShoes);
 });
